0ed265e62d140e996fdfd74b2acd934c7b8180c0
n1e <aricover@foxmail.com>
2018-03-17 13:24:47 +0800

    2018年3月17日13点24分

--- python/practice/scrapy_2/novel/novel/spiders/dingdian.py
+++ python/practice/scrapy_2/novel/novel/spiders/dingdian.py
@@ -4,6 +4,11 @@ from scrapy.http import Request
 from ..items import NovelItem
 
 
+def n1e(temp):
+    if temp:
+        return temp[0].encode('utf-8')
+
+
 class DingdianSpider(scrapy.Spider):
     name = 'dingdian'
     allowed_domains = ['23us.so']
@@ -11,24 +16,26 @@ class DingdianSpider(scrapy.Spider):
     bash_html = '.html'
 
     def start_requests(self):
-        for i in range(1,9):
+        for i in range( 1, 9):
             url = self.bash_url + str(i) + '_1' + self.bash_html
             yield Request(url, self.parse)
         yield Request('http://www.23us.so/full.html',self.parse)
 
     def parse(self, response):
-        #novels = response.xpath('//dl[@id="content"]/dd/table/tbody/tr[@bgcolor="#FFFFFF"]')
-        novels = response.xpath('//*[@id="content"]/dd[1]/table/tbody/tr[2]')
-        print novels.extract()
-        links = response.xpath('//dl[@id="content"]/dd[@class="pages"]/div')
-        category = response.xpath(r'//dl[@id="content"]/dt/text()')
-
+        #novels = response.xpath('//dl[@id="content"]/dd/table/tbody/tr[@bgcolor="#FFFFFF"]') no tbody,create by chrome
+        novels = response.xpath('//dl[@id="content"]/dd[1]/table/tr[@bgcolor="#FFFFFF"]')
+        print '[debug_n1e]--->%s' %novels.extract()
+        category = n1e(response.xpath(r'//dl[@id="content"]/dt/text()').extract())
+        print '[debug_n1e]-->%s' %category
+        print '[debug_n1e]-->%s' % novels[0]
+        print '[debug_n1e]-->%s' %novels[0].xpath('.//td[1]').extract()[0].encode('utf-8')
         for i in novels:
             item = NovelItem()
-            item['name'] = i.xpath('/td[1]')
-            item['author'] = i.xpath('/td[3]')
-            item['time'] = i.xpath('/td[5]')
-            item['serialStatus'] = i.xpath('/td[6]')
-            item['serialNums'] = i.xpath('/td[4]')
+            item['name'] = n1e(i.xpath('.//td[1]/a/text()').extract())
+            item['novelUrl'] = n1e(i.xpath('.//td[1]/a/@href').extract())
+            item['author'] = n1e(i.xpath('.//td[3]/text()').extract())
+            item['time'] = n1e(i.xpath('.//td[5]/text()').extract())
+            item['serialStatus'] = n1e(i.xpath('.//td[6]/text()').extract())
+            item['serialNums'] = n1e(i.xpath('.//td[4]/text()').extract())
             item['category'] = category
             yield item
