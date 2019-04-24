4842ff0df581b983a5f196ccda57c9ebad1e487e
n1e <aricover@foxmail.com>
2019-04-24 19:31:32 +0800

    2019年4月24日19:30:59

--- cpp/spin_lock.h
+++ cpp/spin_lock.h
@@ -4,8 +4,8 @@
 #define ATOM_CMP_SET(lock,old,new)\
 	(InterlockedCompareExchange(lock,new,old) == old)
 
-
-
 #else
+#define ATOM_CMP_SET(lock,old,new)\
+	(__sync_bool_compare_and_swap(lock,old,new))
 
 #endif
